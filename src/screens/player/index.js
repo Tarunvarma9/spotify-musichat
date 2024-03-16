e05815c9a6808292fb8526cf04d02a3dd8de21d5
tarun.varma <tarun.varma@effigoglobal.com>
2024-03-16 19:41:17 +0530

    changes commit

--- src/screens/player/index.js
+++ src/screens/player/index.js
@@ -5,25 +5,33 @@ import apiClient from "../../spotify";
 import SongCard from "../../components/songCard";
 import Queue from "../../components/queue";
 import Widgets from "../../components/widgets";
+import { Spotify } from "react-spotify-embed";
 
 export default function Player() {
   const location = useLocation();
 
+  const handleError =()=>{
+    localStorage.removeItem("token")
+      window.location.reload()
+  }
 
 
   return (
     <div className="screen-container flex">
-      {location.state.id &&(
+      {location.state.id ?(
             <iframe
             title="Spotify Embed"
-            src={`https://open.spotify.com/embed/playlist/${location.state.id}?utm_source=generator&theme=0`}
+            src={`https://open.spotify.com/embed/playlist/${location.state.id}?utm_source=generator&theme=1`}
             width="100%"
             height="100%"
             style={{ minHeight: '360px' }}
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
             loading="lazy"
           />
-      )}
+      ):(<>{handleError}
+      
+      </>)
+    }
     </div>
   );
 }
